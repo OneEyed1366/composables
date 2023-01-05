70853c7f038fd62e649a3db618f7a029e0731782
OneEyed1366 <psevdoproger@gmail.com>
2023-01-05 07:22:18 +0300

    feat(ts): update

---
+++ ts/namespaces.d.ts
@@ -0,0 +1,14 @@
+export declare namespace NFoo {
+  interface Foo {
+    foo: string;
+    bar: string;
+  }
+}
+
+export declare namespace NBar {
+  interface Bar {
+    bar: string;
+  }
+}
+
+export as namespace Types;
